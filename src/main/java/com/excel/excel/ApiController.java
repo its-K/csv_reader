b4ebbda5f0708d4165ade3180614cc0946870488
Kishore <kishore4110@gmail.com>
2021-05-24 21:40:57 +0530

    Added file upload

---
+++ src/main/java/com/excel/excel/ApiController.java
@@ -0,0 +1,58 @@
+package com.excel.excel;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import net.minidev.json.JSONObject;
+
+@RestController
+@CrossOrigin
+public class ApiController {
+    
+
+    @GetMapping("/")
+    public String kk(){
+        return "kise";
+    }
+
+    @GetMapping("/kk")
+    public String hel(){
+        return "Hellooo";
+    }
+
+    @PostMapping("/upload")
+    public List<JSONObject> fileupload(@RequestParam("file") MultipartFile file) throws IOException{
+        file.transferTo(new File("/home/kishore/Works/csv_reader/"+file.getOriginalFilename()));
+        List<JSONObject> me=new ArrayList<>();
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("/home/kishore/Works/csv_reader/"+file.getOriginalFilename()))) {
+            List <String> values=new ArrayList<>();
+            String line;
+            if((line = br.readLine())!=null){
+                for(String value:line.split(",")){
+                    values.add(value);
+                }
+            }
+            while ((line = br.readLine()) != null) {
+                JSONObject obj = new JSONObject();
+                String[] columns = line.split(",");
+                for(int index=0;index<columns.length;index++){
+                    obj.put(values.get(index), columns[index]);
+                }
+                me.add(obj);
+            }
+        
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return me;
+    }
+}
